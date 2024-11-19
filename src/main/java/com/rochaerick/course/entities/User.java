f8c2d3f5fe4cb90fcf1b2218103972859d3ff765
erickrocha87 <ericksaggi31@gmail.com>
2024-11-19 16:54:34 -0300

    Test profile, jpa

--- src/main/java/com/rochaerick/course/entities/User.java
+++ src/main/java/com/rochaerick/course/entities/User.java
@@ -2,8 +2,18 @@ package com.rochaerick.course.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // AutoIncrement do ID
     private Long id;
     private String name;
     private String email;
