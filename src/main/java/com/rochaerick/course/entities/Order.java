2c84525ddee4bdacedab3d385a653ffff2a28652
erickrocha87 <ericksaggi31@gmail.com>
2024-11-29 15:28:00 -0300

    OrderStatus

--- src/main/java/com/rochaerick/course/entities/Order.java
+++ src/main/java/com/rochaerick/course/entities/Order.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.rochaerick.course.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -25,6 +26,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne //muitos para um
     @JoinColumn(name = "client_id") //identifica qual a chave estrangeira para join
     private User client;
@@ -33,10 +36,11 @@ public class Order implements Serializable {
 
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, User client, OrderStatus orderStatus) {
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);
     }
 
     public Long getId() {
@@ -61,6 +65,18 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+    
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+        
     }
 
     @Override
